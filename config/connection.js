97241185b0308ecaef03013643249673890b118d
Viren <144032304+viren15152@users.noreply.github.com>
2024-01-09 23:13:59 +0000

    created and written the code for my connection.js

---
+++ config/connection.js
@@ -0,0 +1,35 @@
+// This section of my code is my dotenv import
+require("dotenv").config();
+
+// This section of my code is my sequelize import
+const Sequelize = require("sequelize");
+
+// Allows for environmental variables to be used
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: process.env.DB_HOST || "localhost", // Use DB_HOST if available, otherwise default to "localhost"
+      dialect: "mysql",
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+// Test the connection
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+// Export the sequelize instance and the function to test the connection
+module.exports = {
+  sequelize,
+  testConnection,
+};
+
+
